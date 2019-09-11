f58af5906d021e47ba20d31f72aa3171f4cd43b5
mr0zek <szymon.szylhabel@gmail.com>
2019-09-11 12:44:36 +0200

    update

--- _posts/2019-07-29-Kamperem_na_wschod.md
+++ _posts/2019-07-29-Kamperem_na_wschod.md
@@ -18,4 +18,6 @@ author: # Add name author (optional)
 Wyjechaliśmy z Wrocławia 29 czerwca 2019r. W planie było odwiedzić parki narodowe po wschodniej części polski. Jechaliśmy i spotkała nas straszna burza. Byliśmy przerażeni. Gdy burza ucichła pojechaliśmy  na frytki z McDonalda. Jechaliśmy dalej. Przyjechaliśmy na biwak w Narwiańskim Parku Narodowym w którym skończyliśmy poprzednią wyprawę kamperem kilka miesięcy wcześniej. 
 Poszliśmy się wykąpać a prysznic były tony much i ciem. Potem z tatą poszliśmy szukać bobra którego słyszeliśmy na polu namiotowym. Okazało się że Tata nas wkęcał i to ktoś chrapał w namiocie. 
 
+<div class="gallery" data-date="2019-06-29"></div>
+
 ## Dzień 2
--- assets/js/main.js
+++ assets/js/main.js
@@ -38,9 +38,32 @@ $(function () {
 
     if (searchBox.hasClass('search-active')) {
       searchClose.click(function (e) {
-    		e.preventDefault()
-    		searchBox.removeClass('search-active')
-    	})
+        e.preventDefault()
+        searchBox.removeClass('search-active')
+      })
     }
   })
+
+  const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g // the only difference is the [ at the beginning
+  function extractPhotos(content) {
+    const links = new Set()
+    let match
+    while (match = regex.exec(content)) {
+      links.add(match[1])
+    }
+    return Array.from(links)
+  }
+
+  $('.gallery').html(
+    `<div data-nanogallery2='{
+      "userID": "184392815@N04",
+      "kind": "flickr",
+      "photoset": "72157710794650718",
+      "thumbnailOpenOriginal": true,
+      "thumbnailWidth": "200",
+      "thumbnailAlignment": "center",
+      "thumbnailLabel": {
+        "display": false
+      }
+    }'></div>`);
 })
